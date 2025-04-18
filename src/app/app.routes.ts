cf7a2dd6c00ae1516e9bf23803f43556ae6b7092
sharonlopez19 <sharon.nicolle173@gmail.com>
2025-04-18 16:36:32 -0500

    Creacion de componente form horas extra

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,7 @@ import { HorasExtraComponent } from './pages/horas-extra/horas-extra.component';
 import { NotificacionesComponent } from './pages/notificaciones/notificaciones.component';
 import { FormvacacionesComponent} from './pages/formvacaciones/formvacaciones.component';
 import { NotificacionesAdminComponent } from './pages/notificaciones-admin/notificaciones-admin.component';
+//import { FormhorasExtraComponent} from './pages/formhoras-extra/formhoras-extra.component';
 
 import { AuthGuard } from './guards/auth.guard';
 
@@ -33,7 +34,8 @@ export const routes: Routes = [
       { path: 'horas-extra', component: HorasExtraComponent },
       { path: 'notificaciones', component: NotificacionesComponent },
       { path: 'notificaciones-admin', component: NotificacionesAdminComponent },
-      { path: 'formvacaciones', component: FormvacacionesComponent }
+      { path: 'formvacaciones', component: FormvacacionesComponent },
+      //{ path: 'formhoras-extra', component: FormhorasExtraComponent}
     ]
   },
 
